049c6a6f75a8a4695e733fead44645848f839ae9
ja35obando <ja35obando@gmail.com>
2014-03-04 23:45:35 -0600

    Archivos

---
+++ Scripts/comment.js
@@ -0,0 +1,4 @@
+﻿$(document).ready(function () {
+    $.getScript("http://code.jquery.com/ui/1.9.2/jquery-ui.js").done(function (script, textStatus) { $(".alert-info").alert('close'); $(".alert-success").show(); });
+    $('.nav-pills>li[data-slide-to="2"]').addClass('active');
+});
---
+++ Scripts/index.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+    $('.nav-pills>li[data-slide-to="0"]').addClass('active');
+});
