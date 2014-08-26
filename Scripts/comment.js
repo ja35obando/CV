3ddd3e68deeca581aced6e120b0d3395afa840d4
José Andrés Obando Díaz <ja35obando@gmail.com>
2014-08-26 00:56:49 -0600

    Modificaciones

--- Scripts/comment.js
+++ Scripts/comment.js
@@ -1,4 +1,4 @@
 ﻿$(document).ready(function () {
-    $.getScript("http://code.jquery.com/ui/1.9.2/jquery-ui.js").done(function (script, textStatus) { $(".alert-info").alert('close'); $(".alert-success").show(); });
+    $.getScript("http://code.jquery.com/ui/1.9.2/jquery-ui.js").done(function (script, textStatus) { $(".alert-danger").alert('close'); $(".alert-success").show(); });
     $('.nav-pills>li[data-slide-to="2"]').addClass('active');
 });
